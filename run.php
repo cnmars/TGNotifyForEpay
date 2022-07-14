f90b9eac022cc30adb383b903723cab798ac16af
yangningmou <yangningmou@gmail.com>
2022-07-14 12:09:26 +0800

    第一版

---
+++ run.php
@@ -0,0 +1,47 @@
+<?php
+require_once "./config.php";
+require_once "./src/Medoo.php";
+require_once "./src/Tcurl.php";
+
+use Medoo\Medoo;
+
+//加载数据库和redis
+try{
+	$database = new Medoo([
+    	'database_type' => 'mysql',
+    	'server' => $_ENV["servername"],
+    	'port' => $_ENV["port"],
+    	'database_name' => $_ENV["formname"],
+    	'username' => $_ENV["username"],
+    	'password' => $_ENV["passwd"]
+	]);
+
+	$redis = new Redis();
+    $redis->connect($_ENV["redis_host"], $_ENV["redis_port"]);  
+    $redis->ping();
+
+}catch (Exception $e){
+        echo $e->getMessage();
+        die("Fail to connect database or redis!");
+}
+
+//设置机器人密钥
+Tcurl:SetBotToken($_ENV["bottoken"]);
+for($orderNo = $redis->lPop('orderno');$_ENV["number"] && $orderNo;$_ENV["number"]--){
+
+	$curOrder = $database->select("pay_order",["uid","out_trade_no","type","name","getmoney","endtime","domain"],["trade_no"=>$orderNo]);
+	$tgid = $redis->get($curOrder[0]['uid']);
+
+	if($tgid){
+		$messsage = "收款通知！\n尊敬的用户{$curOrder[0]['uid']}您好！\n内部订单号{$curOrder}\n外部订单号{$curOrder[0]['out_trade_no']}\n到账金额{$curOrder[0]['getmoney']}\n付款时间{$curOrder[0]['endtime']}\n网站{$curOrder[0]['domain']}";
+		Tcurl:Tpost("sendMessage",["chat_id"=>$tgid,"text"=>$messsage]);
+		unset($messsage);
+	}
+	
+	unset($tgid);
+	unset($curOrder);
+	$orderNo = $redis->lPop('orderno');
+}
+//$redis->lPop('key1');
+//$redis->set('key', 'value');
+//$redis->get('key');
---
+++ src/Tcurl.php
@@ -0,0 +1,40 @@
+<?php
+class Turl{
+	private static $TeleUrl = "https://api.telegram.org/bot";
+	private static $BotToken = "";
+
+	public static function Tpost($Method,$Params=null){
+		if(is_null(self::$BotToken)){
+			throw new \Exception("cURL error no BotToken", 700);
+		}
+		if(is_null($Method)){
+			throw new \Exception("cURL error no Method", 700);
+		}
+		$turl = self::$TeleUrl . self::$BotToken . '/' . $Method;
+        $Params = json_encode($Params);
+        $C = curl_init();
+        curl_setopt($C, CURLOPT_URL, $turl);
+        curl_setopt($C, CURLOPT_POST, 1);
+        curl_setopt($C, CURLOPT_HTTPHEADER, ['Content-Type:application/json; charset=utf-8']);
+        curl_setopt($C, CURLOPT_POSTFIELDS, $Params);
+        curl_setopt($C, CURLOPT_SSL_VERIFYPEER, false);
+    	curl_setopt($C, CURLOPT_SSL_VERIFYHOST, false);
+    	curl_setopt($C, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3314.0 Safari/537.36 SE 2.X MetaSr 1.0');
+    	curl_setopt($C, CURLOPT_ENCODING, "gzip");
+    	curl_setopt($C, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($C, CURLOPT_TIMEOUT, 5);
+        curl_setopt($C, CURLOPT_RETURNTRANSFER, 1);
+        if($errno = curl_errno($C)) {
+    		$error_message = curl_strerror($errno);
+    		curl_close($C);
+    		throw new \Exception("cURL error ({$errno}):\n {$error_message}", 701);
+		}
+        $R = curl_exec($C);
+        curl_close($C);
+        return $R;
+	}
+
+	public static function SetBotToken($bottoken){
+		self::$BotToken = $bottoken;
+	}
+}
